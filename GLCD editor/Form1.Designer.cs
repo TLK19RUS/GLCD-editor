256fe6243360e9eade8fbc78476f0ff147c3c83a
Ivanov Ivan <tlk19rus@aol.com>
2021-03-06 20:02:21 +0700

    Добавьте файлы проекта.

---
+++ GLCD editor/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace GLCD_editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(385, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(754, 564);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.WordWrap = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(72, 513);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(34, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "5";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(72, 539);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(34, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "8";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 516);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ширина";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 542);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Высота";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(324, 516);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(43, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = ">";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 12);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(364, 486);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            this.dataGridView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1049, 591);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "load file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(284, 516);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(34, 20);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Text = "24";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(235, 516);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(43, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "<";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(304, 553);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "save char";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(385, 591);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "get array";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1151, 637);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ GLCD editor/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace GLCD_editor
+{
+    public partial class Form1 : Form
+    {
+        private ArrayList font_data;
+        private int font_width;
+        private int font_height;
+        private int byte_count;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = Convert.ToString(int.Parse(textBox4.Text)+1);
+            update_char();
+        }
+
+        private void update_char()
+        {
+            font_width = int.Parse(textBox2.Text);
+            font_height = int.Parse(textBox3.Text);
+
+            if (font_width > 8)
+            {
+                byte_count = 2;
+            }
+            else
+            {
+                byte_count = 1;
+            }
+            dataGridView1.Columns.Clear();
+            for (int i = 1; i <= font_width; i++)
+            {
+                dataGridView1.Columns.Add("cl" + i.ToString(), "cl1" + i.ToString());
+            }
+            for (int i = 0; i < dataGridView1.Columns.Count; i++)
+            {
+                dataGridView1.Columns[i].Width = 15;
+            }
+            dataGridView1.Rows.Clear();
+            dataGridView1.Rows.Add(font_height);
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                dataGridView1.Rows[i].Height = 15;
+            }
+
+            textBox1.Clear();
+
+            int ch = int.Parse(textBox4.Text);
+
+            int fidx = (ch - 1) * font_height * byte_count;
+            for (int i = fidx; i < fidx + font_height * byte_count; i++)
+            {
+                textBox1.Text += "0x" + Convert.ToString((byte)font_data[i], 16).PadLeft(2, '0').ToUpper() + ",";
+            }
+            String byte_str;
+            int ri = 0;
+            textBox1.Text += "\r\n";
+            for (int i = fidx; i < fidx + font_height * byte_count; i += byte_count)
+            {
+                if (byte_count == 2)
+                {
+                    byte_str = Convert.ToString((byte)font_data[i + 1], 2).PadLeft(8, '0') + Convert.ToString((byte)font_data[i], 2).PadLeft(8, '0');
+                }
+                else
+                {
+                    byte_str = Convert.ToString((byte)font_data[i], 2).PadLeft(8, '0');
+                }
+                byte_str = byte_str.Substring(byte_str.Length - font_width);
+                textBox1.Text += byte_str + "\r\n";
+                for (int j = 0; j < byte_str.Length; j++)
+                {
+                    if (byte_str.Substring(j, 1) == "1")
+                    {
+                        dataGridView1.Rows[ri].Cells[j].Style.BackColor = Color.Black;
+                    }
+                    else
+                    {
+                        dataGridView1.Rows[ri].Cells[j].Style.BackColor = Color.White;
+                    }
+                }
+                ri++;
+            }
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                ((DataGridView)sender).SelectedCells[0].Selected = false;
+            }
+            catch { }
+        }
+
+        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (((DataGridView)sender).Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor == Color.Black)
+            {
+                ((DataGridView)sender).Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor = Color.White;
+            }
+            else
+            {
+                ((DataGridView)sender).Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor = Color.Black;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            od.InitialDirectory = Environment.CurrentDirectory;
+            String buf;
+            String load_array="";
+            int cmt_idx;
+            textBox1.Clear();
+            if (od.ShowDialog(this) == DialogResult.OK)
+            {
+                using (StreamReader sr = new StreamReader(od.FileName))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        buf = sr.ReadLine();
+                        if (buf.IndexOf('#') == -1)
+                        {
+                            cmt_idx = buf.IndexOf("//");
+                            if (cmt_idx != -1)
+                            { //есть комментарий
+                                buf = buf.Substring(0, cmt_idx);
+                            }
+                            load_array += buf + "\r\n";
+                        }
+                        else
+                        {
+                            MatchCollection match = Regex.Matches(buf, @"FONT_([0-9]+)X([0-9]+)");
+                            foreach (Match m in match)
+                            {
+                                if (m.Groups.Count == 3)
+                                {
+                                    textBox2.Text = m.Groups[1].Value;
+                                    textBox3.Text = m.Groups[2].Value;
+                                }
+                            }
+                        }
+                    }
+                }
+                load_array = load_array.Substring(load_array.IndexOf('{') + 1, load_array.LastIndexOf('}') - load_array.IndexOf('{') - 1);
+                load_array = load_array.Replace(" ", "").Replace("\r","").Replace("\n","");
+                char[] charSeparators = new char[] { ',' };
+                font_data = new ArrayList();
+                string[] values = load_array.Split(charSeparators);
+                if (values.Count() > 0)
+                {
+                    foreach(String str in values)
+                    {
+                        font_data.Add(Convert.ToByte(str, 16));
+                    }
+                }
+                //MessageBox.Show(font_data.Count.ToString());
+                textBox1.Text = load_array;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = Convert.ToString(int.Parse(textBox4.Text) - 1);
+            update_char();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int ch = int.Parse(textBox4.Text);
+
+            int fidx = (ch - 1) * font_height * byte_count;
+            for (int i = fidx; i < fidx + font_height * byte_count; i++)
+            {
+                textBox1.Text += "0x" + Convert.ToString((byte)font_data[i], 16).PadLeft(2, '0').ToUpper() + ",";
+            }
+            String byte_str;
+            byte nd;
+            textBox1.Text += "\r\n";
+            for (int i = 0; i < font_height* byte_count; i+= byte_count)
+            {
+                byte_str = "";
+                for (int j = 0; j < font_width; j++)
+                {
+                    if (dataGridView1.Rows[i/byte_count].Cells[j].Style.BackColor == Color.Black)
+                    {
+                        byte_str += "1";
+                    }
+                    else
+                    {
+                        byte_str += "0";
+                    }
+                }
+
+                
+                textBox1.Text += byte_str + "\r\n";
+
+                if (byte_count == 2)
+                {
+                    nd = Convert.ToByte(byte_str.Substring(font_width-8), 2);
+                    textBox1.Text += font_data[fidx + i].ToString() + "-"+ nd.ToString();
+                    font_data[fidx + i] = nd;
+                    nd = Convert.ToByte(byte_str.Substring(0, font_width - 8), 2);
+                    textBox1.Text += "," + font_data[fidx + i + 1].ToString() + "-" + nd.ToString() + "\r\n";
+                    font_data[fidx + i + 1] = nd;
+                }
+                else
+                {
+                    nd = Convert.ToByte(byte_str, 2);
+                    font_data[fidx+i] = nd;
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            int fi = 0x20;
+            String buf = "{\r\n";
+
+            for (int i = 0; i < font_data.Count; i+= font_height * byte_count)
+            {
+                for (int j = 0; j < font_height*byte_count; j++)
+                {
+                    buf += "0x"+Convert.ToString((byte)font_data[i+j], 16).ToUpper().PadLeft(2, '0') ;
+                    if (i + j + 1 < font_data.Count)
+                    {
+                        buf += ",";
+                    }
+                }
+                buf += " //  " + "0x" + Convert.ToString(fi,16).ToUpper()  +"\r\n";
+                fi++;
+            }
+
+            buf += "}";
+            textBox1.Text = buf;
+        }
+    }
+}
